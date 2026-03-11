3b89ccf6ee0cd5bd764ff5b14e60811feb8cc312
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-03-11 14:26:07 +0000

    ATO-2385: Add and run linter

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import { includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  includeIgnoreFile(
+    fileURLToPath(new URL(".gitignore", import.meta.url)),
+    "Imported .gitignore patterns"
+  ),
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    ignores: [
+      "eslint.config.ts",
+      "vitest.config.ts",
+      "integration-tests/playwright.config.ts",
+    ],
+  }
+);
