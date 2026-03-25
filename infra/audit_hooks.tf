2c2cb3f01c0c4e63635e76356ea17543d3154b24
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-03-25 14:11:54 +0000

    ATO-2447: add lambda audit hook

--- infra/audit_hooks.tf
+++ infra/audit_hooks.tf
@@ -5,3 +5,11 @@ resource "aws_cloudformation_stack" "infrastructure_audit_hook_stack" {
 
   capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
 }
+
+resource "aws_cloudformation_stack" "lambda_audit_hook_stack" {
+  # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3376906289/Lambda+audit+hook
+  name         = "lambda-audit-hook"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/lambda-audit-hook/template.yaml"
+
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+}
