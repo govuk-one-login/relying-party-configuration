f0fa7bc75596a9ccfd375492ed1a556101c42c03
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-03-25 10:51:05 +0000

    ATO-2446: Add infra for container signer

--- infra/signer.tf
+++ infra/signer.tf
@@ -11,3 +11,17 @@ resource "aws_cloudformation_stack" "signer_stack" {
 
   capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
 }
+
+resource "aws_cloudformation_stack" "container_signer_stack" {
+  count = var.create_build_stacks ? 1 : 0
+  # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3376840727/Container+Signer
+  name         = "container-signer"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/container-signer/template.yaml"
+
+  parameters = {
+    Environment     = var.environment
+    AllowedAccounts = join(",", var.signer_allowed_accounts)
+  }
+
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+}
