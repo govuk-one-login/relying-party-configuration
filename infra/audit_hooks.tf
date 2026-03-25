7d7c63e8c0cd73f2f7b4d6928a0333b023d8927f
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-03-25 13:57:59 +0000

    ATO-2391: add infrastructure audit hook

---
+++ infra/audit_hooks.tf
@@ -0,0 +1,7 @@
+resource "aws_cloudformation_stack" "infrastructure_audit_hook_stack" {
+  # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3375464940/Infrastructure+audit+hook
+  name         = "infrastructure-audit-hook"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/infrastructure-audit-hook/template.yaml"
+
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+}
