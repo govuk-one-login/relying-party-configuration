143293760cbb7f15f18e3c5d9337744b9ac3127d
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-04-13 16:11:11 +0100

    ATO-2428: Create VPC

---
+++ infra/vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_cloudformation_stack" "vpc_stack" {
+  # See https://govukverify.atlassian.net/wiki/spaces/PLAT/pages/3531735041/VPC
+  name         = "vpc"
+  template_url = "https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/vpc/template.yaml"
+  capabilities = ["CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
+
+  parameters = {
+    DynamoDBApiEnabled = "Yes"
+  }
+}
