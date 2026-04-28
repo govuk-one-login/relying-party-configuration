0c72a23a113784133738f3396ec24011aa7db1be
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-04-28 17:41:50 +0100

    ATO-2396: Add tests for updating a client

--- integration-tests/client-service.test.ts
+++ integration-tests/client-service.test.ts
@@ -45,6 +45,7 @@ describe("Client service integration test", async () => {
       expect(actualClient).toBeUndefined();
     });
   });
+
   describe("Get client summaries", () => {
     it("should get first page of client summaries (less clients than page size)", async () => {
       const testClient = createClient("test-client-id");
@@ -153,6 +154,36 @@ describe("Client service integration test", async () => {
 
       await expect(() =>
         clientService.createClientWithId("test-client-id", CLIENT_DEFAULTS),
+      ).rejects.toThrow(ClientServiceError);
+    });
+  });
+
+  describe("Update client", () => {
+    it("should update existing client", async () => {
+      const testClient = createClient("test-client-id");
+      await addClientToDynamo(testClient);
+
+      await clientService.updateClient("test-client-id", {
+        ...CLIENT_DEFAULTS,
+        Scopes: ["openid", "phone", "email"],
+      });
+
+      expect(
+        (
+          await dynamoDocClient.get({
+            TableName: "test-client-registry",
+            Key: { ClientID: "test-client-id" },
+          })
+        ).Item,
+      ).toEqual({
+        ...testClient,
+        Scopes: ["openid", "phone", "email"],
+      });
+    });
+
+    it("should fail to update client if client does not exist", async () => {
+      await expect(() =>
+        clientService.updateClient("test-client-id", CLIENT_DEFAULTS),
       ).rejects.toThrow(ClientServiceError);
     });
   });
