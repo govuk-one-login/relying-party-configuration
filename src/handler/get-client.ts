a6684d2d04c09972a0a54abe490dd28ea13607bc
Craig Earl <craig.earl@digital.cabinet-office.gov.uk>
2026-04-29 15:36:18 +0100

    ATO-2397: Add lambda to get client

---
+++ src/handler/get-client.ts
@@ -0,0 +1,59 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from "aws-lambda";
+import { ClientService } from "../services/client-service";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+
+const clientService = new ClientService(
+  DynamoDBDocument.from(new DynamoDBClient({})),
+);
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  if ("GET" !== event.httpMethod) {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        message: "Method not allowed",
+      }),
+    };
+  }
+  try {
+    const clientId = event.pathParameters?.id;
+    if (!clientId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Client ID path parameter not found",
+        }),
+      };
+    }
+
+    const client = await clientService.getClient(clientId);
+    if (!client) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: `Client not found`,
+        }),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(client),
+      };
+    }
+  } catch (error) {
+    console.log((error as Error).message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Internal server error",
+      }),
+    };
+  }
+};
